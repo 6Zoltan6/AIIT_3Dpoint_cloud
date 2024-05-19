5642f652dc9c4326f1cfc79ccc7fa7948a09ff2c
zorro123q <128977691+zorro123q@users.noreply.github.com>
2024-05-19 21:24:45 +0800

    Update test.py

--- AIIT_3Dpoint_cloud-main/test.py
+++ AIIT_3Dpoint_cloud-main/test.py
@@ -1,41 +1,15 @@
-# 绑定按键
-self.ui.action_16.triggered.connect(self.height_color)
+import open3d as o3d
 
-# 函数实现
-def height_color(self):
-    global VIEW_DICT, PROPERTY_DICT
-    try:
-        item = self.ui.treeWidget.currentItem()
-        vtk_polydata = VIEW_DICT[item.text(0)][2]
-        zmax = max(CURRENT_OBJECT[item.text(0)]['data'][:, 2])
-        zmin = min(CURRENT_OBJECT[item.text(0)]['data'][:, 2])
 
-        height_array = numpy_to_vtk(CURRENT_OBJECT[item.text(0)]['data'][:, 2])
-        vtk_polydata.GetPointData().SetScalars(height_array)
+def convert_ply_to_pcd(ply_file, pcd_file):
+    # 读取PLY文件
+    point_cloud = o3d.io.read_point_cloud(ply_file)
 
-        # 创建一个vtkLookupTable对象，用来设置颜色映射
-        lookup_table = vtk.vtkLookupTable()
-        lookup_table.SetTableRange(zmin, zmax)
-        lookup_table.SetHueRange(0.6667, 0)
-        lookup_table.SetSaturationRange(1, 1)
-        lookup_table.SetValueRange(1, 1)
-        lookup_table.Build()
+    # 保存为PCD文件
+    o3d.io.write_point_cloud(pcd_file, point_cloud)
 
-        vtk_vertex = vtk.vtkVertexGlyphFilter()
-        vtk_vertex.SetInputData(vtk_polydata)
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(vtk_vertex.GetOutputPort())
-        mapper.SetLookupTable(lookup_table)
-        mapper.SetScalarRange(zmin, zmax)
 
-        pointCloudActor = vtk.vtkActor()
-        pointCloudActor.SetMapper(mapper)
-
-        self.ren.RemoveActor(VIEW_DICT[item.text(0)][0])
-        VIEW_DICT[item.text(0)][0] = pointCloudActor
-        self.ren.AddActor(pointCloudActor)
-        self.renWin.Render()
-        # PROPERTY_DICT[item.text(0)]['colors'] = ['colorFunc', lookup_table]
-
-    except:
-        pass
+# 使用示例
+ply_file_path = 'C:\\Users\\N203\Desktop\\bunny (1)\\bunny\data\\bun000.ply'  # 填入ply文件的路径
+pcd_file_path = 'test.pcd'  # 填入pcd文件的路径
+convert_ply_to_pcd(ply_file_path, pcd_file_path)
